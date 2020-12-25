6cbaef1b9878d5c9eb1adfe77192e01309c5d63f
Mahmudul Hasan <mahmudul@iotait.tech>
2020-12-25 20:45:31 +0600

    todo with vanila js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+class task 
+{
+  constructor(task) {
+    this.task = task;
+  }
+}
+
+class UI{
+  // add ad
+}
+
+// Even List
+// Form Submit event
+
+document.getElementById("taskForm").addEventListener("submit", function(e){
+  e.preventDefault();
+})
